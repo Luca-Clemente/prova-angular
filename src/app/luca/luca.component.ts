262e18fae5fd8676fe03e88ae549b486db4062c1
Luca-Clemente <lukpinkman@hotmail.com>
2022-04-08 20:00:52 -0300

    1 file modified, 3 added

---
+++ src/app/luca/luca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-luca',
+  templateUrl: './luca.component.html',
+  styleUrls: ['./luca.component.css']
+})
+export class LucaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
